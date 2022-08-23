f1ea335d9e409f54f853bf5c35bfdfb0578202bb
Paulius Jacikevicius <p.jacikevicius@gmail.com>
2022-08-23 16:42:33 +0300

    Added button to add recipe screen

--- lib/screens/add.recipe.screen.dart
+++ lib/screens/add.recipe.screen.dart
@@ -7,92 +7,112 @@ class RecipeDetails extends StatelessWidget {
   
 final DatabaseServices database = DatabaseServices();
 final StorageServices storage = StorageServices();
+ final _text = TextEditingController();
+ bool _validate = false;
 
   @override
   Widget build(BuildContext context) {
     var  media = MediaQuery.of(context).size;
-    return Scaffold(
-      
-      body:       
-          Stack(
-            clipBehavior: Clip.none,
-            children: [
-              ShaderMask(
-              shaderCallback: (rect) {
-                // ignore: prefer_const_constructors
-                return LinearGradient(
-                  begin: Alignment.center,
-                  end: Alignment.bottomCenter,
-                  colors: [Colors.black, Colors.transparent],
-                ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
-              },
-              blendMode: BlendMode.dstIn,
-              child: Image.asset(
-                'images/plateBlack.jpg',
-                )         
-            ),
-            Positioned(
-              top: media.height * 0.26,
-              left: (media.width - media.width* 0.85) / 2,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(30.0), //clipping the whole widget
-                child: Container(     
-                    height: MediaQuery.of(context).size.height * 0.7, //I adjusted here for responsiveness problems on my device
-                    width: MediaQuery.of(context).size.width * 0.85,
-                    color: Colors.white,
-                    child: LayoutBuilder(
-                      builder: (context, constraint) {
-                        return Stack(
-                          children: [                            
-                            Positioned(
-                              bottom: 0.0,
-                              child: Container(
-                                alignment: Alignment.center,
-                                height: MediaQuery.of(context).size.height * 0.1,
-                                width: constraint.biggest.width,
-                                color: Colors.yellow[700],
-                                // ignore: prefer_const_constructors
-                                child: Text(
-                                  'Add Recipe',                                 
-                                ),
+    return Scaffold(      
+      body: Container(
+              height: media.height,
+               width: media.width,
+               child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  ShaderMask(                   
+                  shaderCallback: (rect) {
+                    // ignore: prefer_const_constructors
+                    return LinearGradient(
+                      begin: Alignment.center,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.black, Colors.transparent],
+                    ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+                  },
+                  blendMode: BlendMode.dstIn,
+                  child: Image.asset(
+                    'images/plateBlack.jpg',
+                    )         
+                ),
+                 Positioned(
+                    top: media.height * 0.26,
+                    left: (media.width - media.width* 0.85) / 2,
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(30.0), //clipping the whole widget
+                        child: Container(     
+                            height: MediaQuery.of(context).size.height * 0.7, //I adjusted here for responsiveness problems on my device
+                            width: MediaQuery.of(context).size.width * 0.85,
+                            color: Colors.white,
+                            child: 
+                              LayoutBuilder(
+                                builder: (context, constraint) {
+                                  return Column(
+                                    children: [
+                                      Expanded(
+                                        flex: 1,                                
+                                          child:                               
+                                            Container(
+                                              width: constraint.biggest.width * 0.85,
+                                              height: constraint.biggest.height * 0.2,
+                                              alignment: Alignment.centerLeft,
+                                              child: TextField(
+                                                      controller: _text,
+                                                      decoration: InputDecoration(
+                                                      labelText: 'Enter the Value',
+                                                      errorText: _validate ? 'Value Can\'t Be Empty' : null,
+                                                  ),                     
+                                              ),
+                                            ),  
+                                          ),
+                                        Expanded(
+                                          flex: 3,                                         
+                                          child: Container(
+                                            color: Colors.grey,
+                                          ),                                         
+                                        ),
+                                        Expanded(
+                                          flex: 2,                                          
+                                          child: Container(
+                                            color: Colors.pink,
+                                          ),                                         
+                                        ),                                       
+                                        Expanded(
+                                        flex:1,                                            
+                                        child: GestureDetector(
+                                                  onTap: () {
+                                                  //_text.text.isEmpty ? _validate = true : _validate = false;   
+                                                  print('Start cooking4');   
+                                                // print('Validate: ${_validate}');      
+                                              },
+                                              child: Container(
+                                                alignment: Alignment.center,
+                                                //height: MediaQuery.of(context).size.height * 0.1,
+                                                // width: constraint.biggest.width,
+                                                color: Colors.yellow[700],
+                                                // ignore: prefer_const_constructors
+                                                child: 
+                                                const Text(
+                                                  'Start Cooking',  
+                                                  // ignore: prefer_const_constructors
+                                                    style: TextStyle(
+                                                    color: Colors.black54,
+                                                    fontSize: 25.0,
+                                                    fontWeight: FontWeight.w600
+                                                    )                                    
+                                                )
+                                              ),
+                                            ), 
+                                      ),
+                                    ],
+                                  );
+                                },
                               ),
-                            ),
-                          ],
-                        );
-                      },
-                    ),
+                          ),
+                      ),
                   ),
-              
-              ),
-            ),
-            // Positioned(
-            //   top: 200,
-            //   left: 18,
-            //   child: 
-            //   Card(
-            //     color: Colors.white70,
-                
-            //       shape: RoundedRectangleBorder(
-            //             borderRadius: BorderRadius.circular(30),
-            //           ),
-            //           child: Container(
-            //             height: 550,
-            //             width: 350,
-                         
-            //             padding: EdgeInsets.only(top: 450),
-            //             child: Container(
-                           
-            //               color: Colors.yellow,
-            //               child: Text('Add Recipe'),
-            //             ),
-            //           )
-                     
-            //   ),
-            // ),
-            ] 
-          ),
-          
-       
-    );
-  }
-}
+                ] 
+               ),
+             ),
+           );
+          }
+        }
